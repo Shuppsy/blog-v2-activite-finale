1560e41ca7bf66896e0074c264fae0a377fa110b
Shupps <bert.formation@gmail.com>
2019-07-01 17:57:52 +0200

    fourth commit

--- src/app/component/new-post/new-post.component.ts
+++ src/app/component/new-post/new-post.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { PostService } from 'src/app/services/post.service';
 import { Router } from '@angular/router';
 import { Post } from 'src/app/models/post.model';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-new-post',
@@ -22,7 +23,8 @@ export class NewPostComponent implements OnInit {
   initForm() {
     this.postForm = this.formBuilder.group({
       title: ['', Validators.required],
-      content: ['', Validators.required]
+      content: ['', Validators.required],
+      mailUser: ['']
     });
   }
 
@@ -31,7 +33,7 @@ export class NewPostComponent implements OnInit {
     const content = this.postForm.get('content').value;
     const loveIts = 0;
     const createdAt: number = Date.now();
-    const newPost = new Post(title, content, createdAt, loveIts);
+    const newPost = new Post(title, content, createdAt, loveIts, firebase.auth().currentUser.email);
     this.postService.createNewPost(newPost);
     this.router.navigate(['/posts']);
   }
--- src/app/models/post.model.ts
+++ src/app/models/post.model.ts
@@ -1,5 +1,5 @@
 export class Post {
-  constructor(public title: string, public content: string, public createdAt: number, public loveIts: number) {
+  constructor(public title: string, public content: string, public createdAt: number, public loveIts: number, public mailUser: string) {
     this.createdAt = Date.now();
     this.loveIts = 0;
   }
